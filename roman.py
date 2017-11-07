3047418fab7db23aabfb5045430405af2071ca8a
James Prior <jep200404@columbus.rr.com>
2017-11-07 00:10:55 -0500

    roman.py: Reorder thousands to be first.

--- roman.py
+++ roman.py
@@ -4,27 +4,10 @@ from collections import Counter
 class Roman:
     minimum = 1
 
-    units = {
-        'I': 1,
-        'II': 2,
-        'III': 3,
-        'IV': 4,
-        'V': 5,
-        'VI': 6,
-        'VII': 7,
-        'VIII': 8,
-        'IX': 9,
-    }
-    tens = {
-        'X': 10,
-        'XX': 20,
-        'XXX': 30,
-        'XL': 40,
-        'L': 50,
-        'LX': 60,
-        'LXX': 70,
-        'LXXX': 80,
-        'XC': 90,
+    thousands = {
+        'M': 1000,
+        'MM': 2000,
+        'MMM': 3000,
     }
     hundreds = {
         'C': 100,
@@ -37,23 +20,42 @@ class Roman:
         'DCCC': 800,
         'CM': 900,
     }
-    thousands = {
-        'M': 1000,
-        'MM': 2000,
-        'MMM': 3000,
+    tens = {
+        'X': 10,
+        'XX': 20,
+        'XXX': 30,
+        'XL': 40,
+        'L': 50,
+        'LX': 60,
+        'LXX': 70,
+        'LXXX': 80,
+        'XC': 90,
+    }
+    units = {
+        'I': 1,
+        'II': 2,
+        'III': 3,
+        'IV': 4,
+        'V': 5,
+        'VI': 6,
+        'VII': 7,
+        'VIII': 8,
+        'IX': 9,
     }
     combined = {}
-    for d in units, tens, hundreds, thousands:
+    for d in thousands, hundreds, tens, units:
         combined.update(d)
 
-    units_pattern = '(?P<units>' + '|'.join(units) + ')?'
-    tens_pattern = '(?P<tens>' + '|'.join(tens) + ')?'
-    hundreds_pattern = '(?P<hundreds>' + '|'.join(hundreds) + ')?'
     thousands_pattern = '(?P<thousands>' + '|'.join(thousands) + ')?'
+    hundreds_pattern = '(?P<hundreds>' + '|'.join(hundreds) + ')?'
+    tens_pattern = '(?P<tens>' + '|'.join(tens) + ')?'
+    units_pattern = '(?P<units>' + '|'.join(units) + ')?'
     pattern = re.compile(
         '^' +
-        thousands_pattern + hundreds_pattern +
-        tens_pattern + units_pattern +
+        thousands_pattern +
+        hundreds_pattern +
+        tens_pattern +
+        units_pattern +
         '$')
 
     def __init__(self, roman_numeral):
