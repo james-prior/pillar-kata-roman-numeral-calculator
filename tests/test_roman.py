c6a9614ff9db923e2a2e39e2d50c48951c517941
James Prior <jep200404@columbus.rr.com>
2017-11-08 04:05:20 -0500

    tests/test_roman.py: Test that 0 input raises ValueError.

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -104,7 +104,7 @@ bad_romans_expected_errors = (
 
     ('ONE', ValueError), # not roman
     # (4000, ValueError), # too big
-    # (0, ValueError), # too small
+    (0, ValueError), # too small
     # (None, TypeError),
     # (float('nan'), TypeError),
     # (1j, TypeError),
