30d9580f4a83461f4f33d503a1f5734c814d1210
James Prior <jep200404@columbus.rr.com>
2017-11-08 02:50:57 -0500

    roman.py tests/test_roman.py: Test for additive letters.

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -4,52 +4,29 @@ from roman import Roman
 
 '''
     4 IV
-    6 VI
-    7 VII
-    8 VIII
     9 IX
-    11 XI
-    12 XII
-    13 XIII
     14 XIV
-    15 XV
-    16 XVI
-    17 XVII
-    18 XVIII
     19 XIX
     29 XXIX
-    38 XXXVIII
     40 XL
     41 XLI
     44 XLIV
     49 XLIX
-    56 LVI
-    65 LXV
-    68 LXVIII
     74 LXXIV
-    83 LXXXIII
     89 LXXXIX
     92 XCII
     99 XCIX
-    123 CXXIII
     199 CXCIX
     234 CCXXXIV
     345 CCCXLV
-    383 CCCLXXXIII
     456 CDLVI
     499 CDXCIX
-    567 DLXVII
-    678 DCLXXVIII
     789 DCCLXXXIX
-    888 DCCCLXXXVIII
     890 DCCCXC
     999 CMXCIX
     901 CMI
     1098 MXCVIII
-    1883 MDCCCLXXXIII
     2109 MMCIX
-    3210 MMMCCX
-    3883 MMMDCCCLXXXIII
     3999 MMMCMXCIX
 '''
 
@@ -58,17 +35,40 @@ int_string_romans = '''
     2 II
     3 III
     5 V
+    6 VI
+    7 VII
+    8 VIII
     10 X
+    11 XI
+    12 XII
+    13 XIII
+    15 XV
+    16 XVI
+    17 XVII
+    18 XVIII
     20 XX
     30 XXX
+    38 XXXVIII
     50 L
+    56 LVI
+    65 LXV
+    68 LXVIII
+    83 LXXXIII
     100 C
+    123 CXXIII
     200 CC
     300 CCC
+    383 CCCLXXXIII
     500 D
+    567 DLXVII
+    678 DCLXXVIII
+    888 DCCCLXXXVIII
     1000 M
+    1883 MDCCCLXXXIII
     2000 MM
     3000 MMM
+    3210 MMMCCX
+    3883 MMMDCCCLXXXIII
 '''.strip().split('\n')
 expected_roman_strings = (s.strip().split() for s in int_string_romans)
 roman_expected_ints = [
