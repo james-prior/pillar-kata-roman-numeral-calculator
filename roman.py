f425d1db37ed8f24ea8aa7cfb815e322ebf9529b
James Prior <jep200404@columbus.rr.com>
2017-11-08 02:38:35 -0500

    roman.py tests/test_roman.py: Start over with logic for parsing.

--- roman.py
+++ roman.py
@@ -2,100 +2,102 @@ import re
 from collections import Counter
 
 class Roman:
-    minimum = 1
-    maximum = 3999
+    # minimum = 1
+    # maximum = 3999
 
-    def _make_list_of_roman_digits(s):
-        return [''] + s.split()
+    # def _make_list_of_roman_digits(s):
+    #     return [''] + s.split()
 
-    roman_units = _make_list_of_roman_digits('I II III IV V VI VII VIII IX')
-    roman_tens = _make_list_of_roman_digits('X XX XXX XL L LX LXX LXXX XC')
-    roman_hundreds = _make_list_of_roman_digits('C CC CCC CD D DC DCC DCCC CM')
-    roman_thousands = _make_list_of_roman_digits('M MM MMM')
+    # roman_units = _make_list_of_roman_digits('I II III IV V VI VII VIII IX')
+    # roman_tens = _make_list_of_roman_digits('X XX XXX XL L LX LXX LXXX XC')
+    # roman_hundreds = _make_list_of_roman_digits('C CC CCC CD D DC DCC DCCC CM')
+    # roman_thousands = _make_list_of_roman_digits('M MM MMM')
 
-    def _make_value_of_roman_digits(roman_digits, scaler):
-        return {
-            roman_digit: i * scaler
-            for i, roman_digit in enumerate(roman_digits)
-            if roman_digit
-        }
+    # def _make_value_of_roman_digits(roman_digits, scaler):
+    #     return {
+    #         roman_digit: i * scaler
+    #         for i, roman_digit in enumerate(roman_digits)
+    #         if roman_digit
+    #     }
 
-    value_of_roman_thousands = _make_value_of_roman_digits(
-        roman_thousands, 1000)
-    value_of_roman_hundreds = _make_value_of_roman_digits(roman_hundreds, 100)
-    value_of_roman_tens = _make_value_of_roman_digits(roman_tens, 10)
-    value_of_roman_units = _make_value_of_roman_digits(roman_units, 1)
-    value_dicts = (
-        value_of_roman_thousands,
-        value_of_roman_hundreds,
-        value_of_roman_tens,
-        value_of_roman_units,
-    )
-    value_of_roman_digits = {}
-    for d in value_dicts:
-        value_of_roman_digits.update(d)
+    # value_of_roman_thousands = _make_value_of_roman_digits(
+    #     roman_thousands, 1000)
+    # value_of_roman_hundreds = _make_value_of_roman_digits(roman_hundreds, 100)
+    # value_of_roman_tens = _make_value_of_roman_digits(roman_tens, 10)
+    # value_of_roman_units = _make_value_of_roman_digits(roman_units, 1)
+    # value_dicts = (
+    #     value_of_roman_thousands,
+    #     value_of_roman_hundreds,
+    #     value_of_roman_tens,
+    #     value_of_roman_units,
+    # )
+    # value_of_roman_digits = {}
+    # for d in value_dicts:
+    #     value_of_roman_digits.update(d)
 
-    def _make_digits_pattern(roman_digits):
-        return '(' + '|'.join(roman_digits) + ')?'
+    # def _make_digits_pattern(roman_digits):
+    #     return '(' + '|'.join(roman_digits) + ')?'
 
-    pattern = re.compile(
-        '^' +
-        ''.join(map(_make_digits_pattern, value_dicts)) +
-        '$')
+    # pattern = re.compile(
+    #     '^' +
+    #     ''.join(map(_make_digits_pattern, value_dicts)) +
+    #     '$')
 
     def __init__(self, roman_numeral_or_x):
-        try:
-            self.value = int(roman_numeral_or_x)
-        except ValueError:
-            roman_numeral = roman_numeral_or_x
-        else:
-            if self.minimum <= self.value <= self.maximum:
-                return
-            raise ValueError
+        self.value = 1
+        # self.value = None
+    #     try:
+    #         self.value = int(roman_numeral_or_x)
+    #     except ValueError:
+    #         roman_numeral = roman_numeral_or_x
+    #     else:
+    #         if self.minimum <= self.value <= self.maximum:
+    #             return
+    #         raise ValueError
 
-        m = re.match(self.pattern, roman_numeral)
-        if not m:
-            raise ValueError
-        self.value = sum(
-            self.value_of_roman_digits[s]
-            for s in m.groups()
-            if s
-        )
-        if self.value < self.minimum:
-            raise ValueError
+    #     m = re.match(self.pattern, roman_numeral)
+    #     if not m:
+    #         raise ValueError
+    #     self.value = sum(
+    #         self.value_of_roman_digits[s]
+    #         for s in m.groups()
+    #         if s
+    #     )
+    #     if self.value < self.minimum:
+    #         raise ValueError
 
     def get_value(self):
         return self.value
 
-    def __add__(self, other):
-        value = self.value + other.value
-        if value > self.maximum:
-            raise OverflowError
-        return Roman(value)
+    # def __add__(self, other):
+    #     value = self.value + other.value
+    #     if value > self.maximum:
+    #         raise OverflowError
+    #     return Roman(value)
 
-    def __sub__(self, other):
-        value = self.value - other.value
-        if value < self.minimum:
-            raise OverflowError
-        return Roman(value)
+    # def __sub__(self, other):
+    #     value = self.value - other.value
+    #     if value < self.minimum:
+    #         raise OverflowError
+    #     return Roman(value)
 
-    def __str__(self):
-        value = self.value
-        reversed_digits = map(int, reversed(str(self.value)))
-        roman_dicts = [
-            self.roman_units,
-            self.roman_tens,
-            self.roman_hundreds,
-            self.roman_thousands,
-        ]
-        terms = [
-            d[i]
-            for i, d in zip(reversed_digits, roman_dicts)
-        ]
-        return ''.join(reversed(terms))
+    # def __str__(self):
+    #     value = self.value
+    #     reversed_digits = map(int, reversed(str(self.value)))
+    #     roman_dicts = [
+    #         self.roman_units,
+    #         self.roman_tens,
+    #         self.roman_hundreds,
+    #         self.roman_thousands,
+    #     ]
+    #     terms = [
+    #         d[i]
+    #         for i, d in zip(reversed_digits, roman_dicts)
+    #     ]
+    #     return ''.join(reversed(terms))
 
-    def __repr__(self):
-        return "{name}({value})".format(
-            name=self.__class__.__name__,
-            value=repr(str(self)),
-        )
+    # def __repr__(self):
+    #     return "{name}({value})".format(
+    #         name=self.__class__.__name__,
+    #         value=repr(str(self)),
+    #     )
--- tests/test_roman.py
+++ tests/test_roman.py
@@ -2,8 +2,7 @@ import pytest
 
 from roman import Roman
 
-int_string_romans = '''
-    1 I
+'''
     2 II
     3 III
     4 IV
@@ -66,6 +65,10 @@ int_string_romans = '''
     3210 MMMCCX
     3883 MMMDCCCLXXXIII
     3999 MMMCMXCIX
+'''
+
+int_string_romans = '''
+    1 I
 '''.strip().split('\n')
 expected_roman_strings = (s.strip().split() for s in int_string_romans)
 roman_expected_ints = [
@@ -76,130 +79,130 @@ def test_known_number_returns_expected(roman, expected):
     assert expected == Roman(roman).get_value()
 
 
-bad_romans_expected_errors = (
-    ('', ValueError),
-    ('IL', ValueError),
-    ('XD', ValueError),
-    ('IIII', ValueError),
-    ('IIIII', ValueError),
-    ('VV', ValueError),
-    ('CCCC', ValueError),
-    ('DD', ValueError),
-    ('MMMM', ValueError), # too big value
-    ('ONE', ValueError), # not roman
-    (4000, ValueError), # too big
-    (0, ValueError), # too small
-    (None, TypeError),
-    (float('nan'), TypeError),
-    (1j, TypeError),
-)
-@pytest.mark.parametrize(
-    'bad_roman, expected_exception', bad_romans_expected_errors)
-def test_bad_roman_raises_expected_exception(
-        bad_roman, expected_exception):
-    with pytest.raises(expected_exception):
-        _ = Roman(bad_roman)
-
-
-number_expected_roman_strings = (
-    s.strip().split() for s in int_string_romans)
-int_expected_roman_numerals = [
-    (int(x), roman)
-    for x, roman in number_expected_roman_strings]
-@pytest.mark.parametrize(
-    'number, expected_roman_numeral', int_expected_roman_numerals)
-def test_str_returns_expected(number, expected_roman_numeral):
-    assert expected_roman_numeral == str(Roman(number))
-
-
-addends_sums_lines = '''
-    I I II
-    I II III
-    II III V
-    III V VIII
-    V VIII XIII
-    VIII XIII XXI
-    XIII XXI XXXIV
-    XXI XXXIV LV
-    XXXIV LV LXXXIX
-    LV LXXXIX CXLIV
-    LXXXIX CXLIV CCXXXIII
-    CXLIV CCXXXIII CCCLXXVII
-    CCXXXIII CCCLXXVII DCX
-    CCCLXXVII DCX CMLXXXVII
-    DCX CMLXXXVII MDXCVII
-    CMLXXXVII MDXCVII MMDLXXXIV
-    MMMCMXCVIII I MMMCMXCIX
-'''.strip().split('\n')
-addends_sums = (
-    s.strip().split() for s in addends_sums_lines)
-@pytest.mark.parametrize('addends_sum', addends_sums)
-def test_adding(addends_sum):
-    addend1, addend2, expected_sum = addends_sum
-    assert expected_sum == str(Roman(addend1) + Roman(addend2))
-
-
-bad_adds_expected_errors = (
-    ('MM', 'MM', OverflowError),
-    ('MMMCMXCIX', 'I', OverflowError),
-    ('I', 'MMMCMXCIX', OverflowError),
-    ('MMMCMXCIX', 'MMMCMXCIX', OverflowError),
-)
-@pytest.mark.parametrize(
-    'addend1, addend2, expected_exception', bad_adds_expected_errors)
-def test_bad_add_raises_expected_exception(
-        addend1, addend2, expected_exception):
-    with pytest.raises(expected_exception):
-        Roman(addend1) + Roman(addend2)
-
-
-subs_sums_lines = '''
-    II I I
-    III I II
-    V II III
-    VIII III V
-    XIII V VIII
-    XXI VIII XIII
-    XXXIV XIII XXI
-    LV XXI XXXIV
-    LXXXIX XXXIV LV
-    CXLIV LV LXXXIX
-    CCXXXIII LXXXIX CXLIV
-    CCCLXXVII CXLIV CCXXXIII
-    DCX CCXXXIII CCCLXXVII
-    CMLXXXVII CCCLXXVII DCX
-    MDXCVII DCX CMLXXXVII
-    MMDLXXXIV CMLXXXVII MDXCVII
-    MMMCMXCIX MMMCMXCVIII I
-'''.strip().split('\n')
-subs_sums = (
-    s.strip().split() for s in subs_sums_lines)
-@pytest.mark.parametrize('subs_sum', subs_sums)
-def test_subtracting(subs_sum):
-    sub1, sub2, expected_sum = subs_sum
-    assert expected_sum == str(Roman(sub1) - Roman(sub2))
-
-
-bad_subs_expected_errors = (
-    ('I', 'I', OverflowError),
-    ('I', 'II', OverflowError),
-    ('M', 'MM', OverflowError),
-    ('MMMCMXCVIII', 'MMMCMXCIX', OverflowError),
-    ('MMMCMXCIX', 'MMMCMXCIX', OverflowError),
-)
-@pytest.mark.parametrize(
-    'sub1, sub2, expected_exception', bad_subs_expected_errors)
-def test_bad_sub_raises_expected_exception(
-        sub1, sub2, expected_exception):
-    with pytest.raises(expected_exception):
-        Roman(sub1) - Roman(sub2)
-
-
-roman_expected_repr = (
-    (Roman('I'), "Roman('I')"),
-    (Roman('II'), "Roman('II')"),
-    (Roman('MMMCMXCIX'), "Roman('MMMCMXCIX')"),
-)
-@pytest.mark.parametrize('roman, expected_repr', roman_expected_repr)
-def test_repr(roman, expected_repr):
-    assert expected_repr == repr(roman)
+# bad_romans_expected_errors = (
+#     ('', ValueError),
+#     ('IL', ValueError),
+#     ('XD', ValueError),
+#     ('IIII', ValueError),
+#     ('IIIII', ValueError),
+#     ('VV', ValueError),
+#     ('CCCC', ValueError),
+#     ('DD', ValueError),
+#     ('MMMM', ValueError), # too big value
+#     ('ONE', ValueError), # not roman
+#     (4000, ValueError), # too big
+#     (0, ValueError), # too small
+#     (None, TypeError),
+#     (float('nan'), TypeError),
+#     (1j, TypeError),
+# )
+# @pytest.mark.parametrize(
+#     'bad_roman, expected_exception', bad_romans_expected_errors)
+# def test_bad_roman_raises_expected_exception(
+#         bad_roman, expected_exception):
+#     with pytest.raises(expected_exception):
+#         _ = Roman(bad_roman)
+# 
+# 
+# number_expected_roman_strings = (
+#     s.strip().split() for s in int_string_romans)
+# int_expected_roman_numerals = [
+#     (int(x), roman)
+#     for x, roman in number_expected_roman_strings]
+# @pytest.mark.parametrize(
+#     'number, expected_roman_numeral', int_expected_roman_numerals)
+# def test_str_returns_expected(number, expected_roman_numeral):
+#     assert expected_roman_numeral == str(Roman(number))
+# 
+# 
+# addends_sums_lines = '''
+#     I I II
+#     I II III
+#     II III V
+#     III V VIII
+#     V VIII XIII
+#     VIII XIII XXI
+#     XIII XXI XXXIV
+#     XXI XXXIV LV
+#     XXXIV LV LXXXIX
+#     LV LXXXIX CXLIV
+#     LXXXIX CXLIV CCXXXIII
+#     CXLIV CCXXXIII CCCLXXVII
+#     CCXXXIII CCCLXXVII DCX
+#     CCCLXXVII DCX CMLXXXVII
+#     DCX CMLXXXVII MDXCVII
+#     CMLXXXVII MDXCVII MMDLXXXIV
+#     MMMCMXCVIII I MMMCMXCIX
+# '''.strip().split('\n')
+# addends_sums = (
+#     s.strip().split() for s in addends_sums_lines)
+# @pytest.mark.parametrize('addends_sum', addends_sums)
+# def test_adding(addends_sum):
+#     addend1, addend2, expected_sum = addends_sum
+#     assert expected_sum == str(Roman(addend1) + Roman(addend2))
+# 
+# 
+# bad_adds_expected_errors = (
+#     ('MM', 'MM', OverflowError),
+#     ('MMMCMXCIX', 'I', OverflowError),
+#     ('I', 'MMMCMXCIX', OverflowError),
+#     ('MMMCMXCIX', 'MMMCMXCIX', OverflowError),
+# )
+# @pytest.mark.parametrize(
+#     'addend1, addend2, expected_exception', bad_adds_expected_errors)
+# def test_bad_add_raises_expected_exception(
+#         addend1, addend2, expected_exception):
+#     with pytest.raises(expected_exception):
+#         Roman(addend1) + Roman(addend2)
+# 
+# 
+# subs_sums_lines = '''
+#     II I I
+#     III I II
+#     V II III
+#     VIII III V
+#     XIII V VIII
+#     XXI VIII XIII
+#     XXXIV XIII XXI
+#     LV XXI XXXIV
+#     LXXXIX XXXIV LV
+#     CXLIV LV LXXXIX
+#     CCXXXIII LXXXIX CXLIV
+#     CCCLXXVII CXLIV CCXXXIII
+#     DCX CCXXXIII CCCLXXVII
+#     CMLXXXVII CCCLXXVII DCX
+#     MDXCVII DCX CMLXXXVII
+#     MMDLXXXIV CMLXXXVII MDXCVII
+#     MMMCMXCIX MMMCMXCVIII I
+# '''.strip().split('\n')
+# subs_sums = (
+#     s.strip().split() for s in subs_sums_lines)
+# @pytest.mark.parametrize('subs_sum', subs_sums)
+# def test_subtracting(subs_sum):
+#     sub1, sub2, expected_sum = subs_sum
+#     assert expected_sum == str(Roman(sub1) - Roman(sub2))
+# 
+# 
+# bad_subs_expected_errors = (
+#     ('I', 'I', OverflowError),
+#     ('I', 'II', OverflowError),
+#     ('M', 'MM', OverflowError),
+#     ('MMMCMXCVIII', 'MMMCMXCIX', OverflowError),
+#     ('MMMCMXCIX', 'MMMCMXCIX', OverflowError),
+# )
+# @pytest.mark.parametrize(
+#     'sub1, sub2, expected_exception', bad_subs_expected_errors)
+# def test_bad_sub_raises_expected_exception(
+#         sub1, sub2, expected_exception):
+#     with pytest.raises(expected_exception):
+#         Roman(sub1) - Roman(sub2)
+# 
+# 
+# roman_expected_repr = (
+#     (Roman('I'), "Roman('I')"),
+#     (Roman('II'), "Roman('II')"),
+#     (Roman('MMMCMXCIX'), "Roman('MMMCMXCIX')"),
+# )
+# @pytest.mark.parametrize('roman, expected_repr', roman_expected_repr)
+# def test_repr(roman, expected_repr):
+#     assert expected_repr == repr(roman)
