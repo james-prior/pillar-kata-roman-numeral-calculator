6e9a0ce823a2a5d1b0b0df4c929114c8109b3af8
James Prior <jep200404@columbus.rr.com>
2017-11-08 04:39:40 -0500

    roman.py: Refactor: Remove commented code.

--- roman.py
+++ roman.py
@@ -13,36 +13,6 @@ class Roman:
     roman_hundreds = _make_list_of_roman_digits('C CC CCC CD D DC DCC DCCC CM')
     roman_thousands = _make_list_of_roman_digits('M MM MMM')
 
-    # def _make_value_of_roman_digits(roman_digits, scaler):
-    #     return {
-    #         roman_digit: i * scaler
-    #         for i, roman_digit in enumerate(roman_digits)
-    #         if roman_digit
-    #     }
-
-    # value_of_roman_thousands = _make_value_of_roman_digits(
-    #     roman_thousands, 1000)
-    # value_of_roman_hundreds = _make_value_of_roman_digits(roman_hundreds, 100)
-    # value_of_roman_tens = _make_value_of_roman_digits(roman_tens, 10)
-    # value_of_roman_units = _make_value_of_roman_digits(roman_units, 1)
-    # value_dicts = (
-    #     value_of_roman_thousands,
-    #     value_of_roman_hundreds,
-    #     value_of_roman_tens,
-    #     value_of_roman_units,
-    # )
-    # value_of_roman_digits = {}
-    # for d in value_dicts:
-    #     value_of_roman_digits.update(d)
-
-    # def _make_digits_pattern(roman_digits):
-    #     return '(' + '|'.join(roman_digits) + ')?'
-
-    # pattern = re.compile(
-    #     '^' +
-    #     ''.join(map(_make_digits_pattern, value_dicts)) +
-    #     '$')
-
     value_of_roman_letters = {
         'I': 1,
         'V': 5,
@@ -109,25 +79,6 @@ class Roman:
             old_letter_value = letter_value
         self.value = value
         # self.value = None
-    #     try:
-    #         self.value = int(roman_numeral_or_x)
-    #     except ValueError:
-    #         roman_numeral = roman_numeral_or_x
-    #     else:
-    #         if self.minimum <= self.value <= self.maximum:
-    #             return
-    #         raise ValueError
-
-    #     m = re.match(self.pattern, roman_numeral)
-    #     if not m:
-    #         raise ValueError
-    #     self.value = sum(
-    #         self.value_of_roman_digits[s]
-    #         for s in m.groups()
-    #         if s
-    #     )
-    #     if self.value < self.minimum:
-    #         raise ValueError
 
     def get_value(self):
         return self.value
