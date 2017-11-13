f04c445505573146292faeed74c0c1899d60f1da
James Prior <jep200404@columbus.rr.com>
2017-11-13 15:32:29 -0500

    src/roman.h: Fix bug: uint -> unsigned
    I was surprised that the bug worked.

--- src/roman.h
+++ src/roman.h
@@ -6,7 +6,7 @@ struct roman_struct {
 };
 
 struct roman_struct *new_roman(char *roman_numeral);
-struct roman_struct *new_roman_from_uint(uint value);
+struct roman_struct *new_roman_from_uint(unsigned value);
 unsigned get_roman_value(struct roman_struct *roman);
 void free_roman(struct roman_struct *m);
 
