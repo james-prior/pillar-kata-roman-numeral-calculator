16e8b0b5eb0a4c816c4bd8eca06ce499a7922d7f
James Prior <jep200404@columbus.rr.com>
2017-11-27 13:10:09 -0500

    library/src/roman.h: Decreased length of roman_numeral from 21 to 4+4+4+3+1.

--- library/src/roman.h
+++ library/src/roman.h
@@ -1,7 +1,7 @@
 #if !defined(ROMAN_H)
 #define ROMAN_H
 
-typedef char roman_numeral[21];
+typedef char roman_numeral[4+4+4+3+1];
 roman_numeral *add_roman_numerals(
     roman_numeral *sum, char *addend1, char *addend2);
 roman_numeral *subtract_roman_numerals(
