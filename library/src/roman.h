bc68339cf48dbeda8eb924f40816bf2c963473f5
James Prior <jep200404@columbus.rr.com>
2017-11-27 13:13:40 -0500

    library/src/roman.h: Add comment.

--- library/src/roman.h
+++ library/src/roman.h
@@ -2,6 +2,7 @@
 #define ROMAN_H
 
 typedef char roman_numeral[4+4+4+3+1];
+// The following return NULL for any error.
 roman_numeral *add_roman_numerals(
     roman_numeral *sum, char *addend1, char *addend2);
 roman_numeral *subtract_roman_numerals(
