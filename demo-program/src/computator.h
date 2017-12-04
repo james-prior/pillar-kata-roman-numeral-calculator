80d8541f477aaf5a46f8f87f55a46c5c54aa6220
James Prior <jep200404@columbus.rr.com>
2017-12-04 10:53:26 -0500

    demo-program/src/computator.h: Remove unnecessary function prototype.

--- demo-program/src/computator.h
+++ demo-program/src/computator.h
@@ -4,7 +4,6 @@
 // TEST_MAIN is defined by compiler for testing.
 #if defined(TEST_MAIN)
 #define MAIN_FUNCTION testable_main
-int testable_main(int argc, char *argv[]);
 #else
 #define MAIN_FUNCTION main
 #endif
